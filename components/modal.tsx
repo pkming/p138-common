e9ac3753c8d914ce2627f92ec27f32ac9e8191e2
barry <707208214@qq.com>
2025-03-21 18:48:09 +0800

    修改modal

--- components/modal.tsx
+++ components/modal.tsx
@@ -1,24 +1,22 @@
-import { kScreenWidth } from "p138-common/utils/fuc/fc.rn";
-import React, { useState, useEffect } from "react";
-import {
-  TouchableOpacity,
-  Modal,
-  Animated,
-  StyleSheet,
-} from "react-native";
+import { Image } from 'expo-image';
+import { kScreenWidth } from 'p138-common/utils/fuc/fc.rn';
+import React, { useState, useEffect } from 'react';
+import { TouchableOpacity, Modal, Animated, StyleSheet } from 'react-native';
 
 interface CustomModalProps {
   isVisible: boolean;
   onClose?: () => void;
   children: React.ReactNode;
-  position?: "center" | "bottom" | "right";
+  position?: 'center' | 'bottom' | 'right';
+  disabled?: boolean;
 }
 
 const CustomModal: React.FC<CustomModalProps> = ({
   isVisible,
   onClose,
   children,
-  position = "center",
+  position = 'center',
+  disabled = false,
 }) => {
   const [fadeAnim] = useState(new Animated.Value(0)); // 初始透明度为 0
   const [scaleAnim] = useState(new Animated.Value(0.8)); // 初始缩放为 0.8
@@ -81,29 +79,46 @@ const CustomModal: React.FC<CustomModalProps> = ({
     <Modal
       transparent={true}
       visible={isVisible}
-      animationType={position === "bottom" ? "slide" : "fade"}
-      onRequestClose={onClose}
-    >
+      animationType={position === 'bottom' ? 'slide' : 'fade'}
+      onRequestClose={onClose}>
       <TouchableOpacity
+        disabled={disabled}
         style={[
           styles.overlay,
-          { opacity: fadeAnim },
-          position === "bottom" && { justifyContent: "flex-end" },
-          position === "right" && { flexDirection: "row", justifyContent: "flex-end" },
+          { opacity: fadeAnim, width: '100%' },
+          position === 'bottom' && { justifyContent: 'flex-end' },
+          position === 'right' && { flexDirection: 'row', justifyContent: 'flex-end' },
         ]} // 控制背景透明度
         activeOpacity={1}
-        onPress={onClose}
-      >
-        <TouchableOpacity activeOpacity={1} className="justify-center items-center">
+        onPress={onClose}>
+        <TouchableOpacity
+          disabled={disabled}
+          activeOpacity={1}
+          className='justify-center items-center w-full'>
           <Animated.View
             style={[
-              { transform: [{ scale: scaleAnim }] },
-              position === "right" && { transform: [{ translateX: translateXAnim }] },
+              {
+                transform: [{ scale: scaleAnim }],
+                width: '100%',
+                justifyContent: 'center',
+                alignItems: 'center',
+              },
+              position === 'right' && { transform: [{ translateX: translateXAnim }] },
             ]} // 控制模态框缩放和位移
           >
             {children}
           </Animated.View>
         </TouchableOpacity>
+        {disabled && (
+          <TouchableOpacity
+            activeOpacity={1}
+            onPress={onClose}>
+            <Image
+              source={require('src/asset/jpImages/close2.png')}
+              style={{ width: 40, height: 40 }}
+            />
+          </TouchableOpacity>
+        )}
       </TouchableOpacity>
     </Modal>
   );
@@ -111,20 +126,20 @@ const CustomModal: React.FC<CustomModalProps> = ({
 
 const styles = StyleSheet.create({
   overlay: {
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.5)", // 背景颜色
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // 背景颜色
     width: kScreenWidth,
     height: '100%',
   },
   closeButton: {
     paddingVertical: 10,
     paddingHorizontal: 20,
-    backgroundColor: "#007BFF",
+    backgroundColor: '#007BFF',
     borderRadius: 5,
   },
   closeButtonText: {
-    color: "#fff",
+    color: '#fff',
     fontSize: 16,
   },
 });
